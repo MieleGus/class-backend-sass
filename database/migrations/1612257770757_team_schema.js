9e61b0e1fc5e89b892468668b387713facb9379a
Gustavo Fraga <gustavohenriquemiele@live.com>
2021-02-02 07:00:51 -0300

    initializing project with migration

---
+++ database/migrations/1612257770757_team_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TeamSchema extends Schema {
+  up () {
+    this.create('teams', (table) => {
+      table.increments()
+      table.string('name').notNullable()
+      table.integer('user_id').unsigned().notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('teams')
+  }
+}
+
+module.exports = TeamSchema
